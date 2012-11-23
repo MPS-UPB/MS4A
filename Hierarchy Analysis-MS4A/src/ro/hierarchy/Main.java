3b38ca727dcb36aeaabbb6a6a6dee7518fbddcdb
Avner Solomon <ravenx86.underworld@gmail.com>
2012-11-23 18:30:09 +0200

    Creat proces de generare a xml-ului de configurare

--- Hierarchy Analysis-MS4A/src/ro/hierarchy/Main.java
+++ Hierarchy Analysis-MS4A/src/ro/hierarchy/Main.java
@@ -4,6 +4,12 @@
  */
 package ro.hierarchy;
 
+import java.io.IOException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBException;
+
 /**
  *
  * @author Avner Solomon
@@ -14,6 +20,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        // exemplu de creare a fisierului de configurare
+        Vector<String> input = new Vector();
+        input.add("input.xml");
+        
+        ro.hierarchy.execWrapper.Wrapper.setIndentation(true);
+        ro.hierarchy.execWrapper.Wrapper.setInputFiles(input);
+        ro.hierarchy.execWrapper.Wrapper.setOutputFile("output_test.xml");
+        ro.hierarchy.execWrapper.Wrapper.createConfigXML("conf.xml");
+        //sfarsit exemplu
+        
+        
+        try {
+            Process p = Runtime.getRuntime().exec("mock_hierarchy.exe conf.xml");
+            //ro.hierarchy.execWrapper.Wrapper.clean();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
+
---
+++ Hierarchy Analysis-MS4A/src/ro/hierarchy/execWrapper/Wrapper.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.hierarchy.execWrapper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Vector;
+
+
+
+/**
+ *
+ * @author RavenX86
+ */
+public class Wrapper {
+    
+    private static Vector<String> inputFiles;
+    private static String outputFile;
+    private static String configXML;
+    private static boolean indentation = false;
+    
+    /**
+     * Set config file indentation on/of
+     * @param indent boolean representing is indentation should be used or not
+     */
+    public static void setIndentation(boolean indent){
+        indentation = indent;
+    }
+    
+    /**
+     * Set input Files
+     * @param fileNames path to each input file
+     */
+    public static void setInputFiles(Vector<String> fileNames){
+        inputFiles = fileNames;
+    }
+    
+    /**
+     * Set output file
+     * @param fileName path to output file
+     */
+    public static void setOutputFile(String fileName){
+        outputFile = fileName;
+    }
+    
+    private static void writeFile(String path, String text){
+        try{
+            FileWriter fstream = new FileWriter(path);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(text);
+            out.close();
+        }catch (Exception e){//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Create the config XML
+     * @param path path for the config XML
+     */
+    public static void createConfigXML(String path){
+ 
+        configXML = path;
+        
+        String xml = "<?xml version=\"1.0\"?>\n";
+        xml+= "<task>";
+        if(indentation){
+            xml+="\n";
+        }
+        for(int i =0; i<inputFiles.size(); i++){
+            if(indentation){
+                xml+="    ";
+            }
+            xml+= "<inputFile name=\""+inputFiles.get(i) +"\"/>";
+            if(indentation){
+                xml+="\n";
+            }
+        }
+        if(indentation){
+            xml+="    ";
+        }
+        xml+= "<outputFile name=\""+outputFile+"\"/>";
+        if(indentation){
+            xml+="\n";
+        }
+        xml+= "</task>\n";
+        
+        System.out.println(xml);
+        writeFile(path,xml);
+    }
+    
+    /**
+     * Used to delete the config XML after use.
+     */
+    public static void clean(){
+        try{
+            File file = new File(configXML);
+            if(file.delete()){
+                System.out.println(file.getName() + " is deleted!");
+            }else{
+                System.out.println("Delete operation is failed.");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+    	}
+    }
+}
